615a7145681f27c569cd0aeeb192f32c1cc67808
Amr Hamouda <amr.hamouda@gmail.com>
2025-08-02 22:05:41 +0300

    refactor: replace MyApp with Init as the main application widget , add HomeScreen , implements notification , implement battery and add the CircularPercentIndicator

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Welcome to Voltify',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'CustomFont',
+          ),
+        ),
+      ),
+    );
+  }
+}
