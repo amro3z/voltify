4826cd5c800611728bc1c284accc5a27e948f77b
Amr Hamouda <amr.hamouda@gmail.com>
2025-08-03 20:38:09 +0300

    toggle appIsRunning state on button press in HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -173,7 +173,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: ElevatedButton(
               onPressed: () {
                 setState(() {
-                  appIsRunning = true;
+                  appIsRunning = !appIsRunning;
                 });
                 Navigator.push(
                   context,
